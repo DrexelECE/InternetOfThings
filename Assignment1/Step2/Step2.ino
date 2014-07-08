3a9b32e88ba9da72189ecd3d46e8826bdfc19df8
James Kurtz <jkrrv@jkrrv.org>
2014-07-08 19:41:26 -0400

    starting to work with NFC

--- Assignment1/Step2/Step2.ino
+++ Assignment1/Step2/Step2.ino
@@ -1,20 +1,48 @@
+/* ECEC 490 - Internet of Things - Summer 2014
+ * Assignment 1-1
+ * Establishes 2 LED blink patterns and allows the user to
+ * select the pattern via serial communication
+ * James Kurtz and Greg Yeutter */
+
+
+// for NFC
+#include <Wire.h>
 #include <Adafruit_NFCShield_I2C.h>
 
+#define IRQ   (2)
+#define RESET (3)  // Not connected by default on the NFC Shield
+
+Adafruit_NFCShield_I2C nfc(IRQ, RESET);
 
 
-/* ECEC 490 - Internet of Things - Summer 2014
-Assignment 1-1
-Establishes 2 LED blink patterns and allows the user to
-select the pattern via serial communication
-James Kurtz and Greg Yeutter */
+
 
 int led = 13;  // LED Pin 
 char input = '0';
 
 void setup() {
+  
+  /* initialize on-board stuff */
   pinMode(led, OUTPUT);  // initialize the digital pin as an output.
-  Serial.begin(9600);  // start serial port at 9600 bps:
-  Serial.println("Enter the blink sequence number, 1 or 2. To turn off, enter 0.");
+  Serial.begin(115200);  // start serial port 
+  
+  
+  /* Initialize NFC */
+  nfc.begin();
+
+  uint32_t versiondata = nfc.getFirmwareVersion();
+  if (! versiondata) {
+    Serial.print("Didn't find PN53x board");
+    while (1); // halt
+  }
+  
+  Serial.print("Found chip PN5"); Serial.println((versiondata>>24) & 0xFF, HEX); 
+  Serial.print("Firmware ver. "); Serial.print((versiondata>>16) & 0xFF, DEC); 
+  Serial.print('.'); Serial.println((versiondata>>8) & 0xFF, DEC);
+  
+  nfc.SAMConfig();   // configure board to read RFID tags
+  
+  Serial.println("Init complete.");
 }
 
 void blink1() {
@@ -32,6 +60,11 @@ void blink2() {
 }
 
 void loop() {
+  //NFC vars:
+  uint8_t success;
+  uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
+  uint8_t uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
+  
   if (Serial.available() > 0) {
     input = Serial.read();
     Serial.println(input);
@@ -39,7 +72,21 @@ void loop() {
       Serial.println("Unrecognized Character");
     }
   }
-  else if (input == '1') {
+  
+  // Wait for an ISO14443A type cards (Mifare, etc.).  When one is found
+  // 'uid' will be populated with the UID, and uidLength will indicate
+  // if the uid is 4 bytes (Mifare Classic) or 7 bytes (Mifare Ultralight)
+  success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
+  
+  if (success) {
+    if (input='1') {
+      input = '0';
+    } else {
+      input = '1';
+    }
+  }
+  
+  if (input == '1') {
     blink1();
   }
   else if (input == '2') {
