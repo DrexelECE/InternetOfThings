c66adf1bcf549714bb572a6929324a3b8aec26d5
Greg Yeutter <yeutterg@gmail.com>
2014-08-19 20:09:19 -0400

    added arduino code prototype

--- Project/BoardCode/BoardCode.ino
+++ Project/BoardCode/BoardCode.ino
@@ -1,9 +1,27 @@
-void setup() {
-  // put your setup code here, to run once:
+int magnetPin = 2; // Interrupt on pin 2? (interrupt 0?) Check board docs
+unsigned long timeElapsed = 0;
+unsigned long lastMicros = 0;
+volatile int magnetPasses = 0;
 
+void setup() {
+  attachInterrupt(0,magnetTick,RISING); // RISING assumes NO sensor, change to FALLING for NC
+}
+
+void magnetTick() {
+  magnetPasses++;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (magnetPasses >= 10) {
+    transmit();
+    timeElapsed = 0;
+  }   
+}
 
+void transmit() {
+  unsigned long currentMicros = micros() - lastMicros;
+  lastMicros = micros();
+  // TODO transmit code here
+  
+  
 }
